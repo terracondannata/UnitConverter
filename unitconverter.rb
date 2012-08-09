0bdc21d9d0fac3d23f5b94b9ee647cab6721d730
jenna <jenna.hildebrand@gmail.com>
2012-08-09 11:34:28 -0700

    Added conversion to butts

--- unitconverter.rb
+++ unitconverter.rb
@@ -1,9 +1,11 @@
 class Converter
 	def initialize
 		@inches = 0.393701
-		#number of inches in a centimeter
+		# number of inches in a centimeter
 		@centimeters = 2.54
-		#number of centimeters in an inch	
+		# number of centimeters in an inch	
+		@butts = 16
+		# number of butts in an inch
 	end
 
 	def convert_centimeters_to_inches(centimeters)
@@ -12,6 +14,14 @@ class Converter
 
 	def convert_inches_to_centimeters(inches)
 		inches * @centimeters
+	end
+
+	def convert_butts_to_inches(inches)
+		inches * @butts
+	end
+
+	def convert_inches_to_butts(butts)
+		butts * @inches
 	end
 end
 
@@ -22,9 +32,10 @@ puts "I can convert centimeters to inches, and vice versa."
 puts "Would you like to convert from"
 puts "(i) inches to centimeters?"
 puts "(c) centimeters to inches?"
+puts "(b) butts to inches?"
 print prompt
 unit = gets.chomp
-if unit != "i" && unit != "c"
+if unit != "i" && unit != "c" && unit != "b"
 	puts "I don't understand. Exiting."
 	exit
 end
@@ -40,6 +51,9 @@ if unit == "i"
 elsif unit == "c"
 	converter = Converter.new
 	puts converter.convert_centimeters_to_inches(how_many)
+elsif unit == "b"
+	converter = Converter.new
+	puts converter.convert_butts_to_inches(how_many)
 end
 
 puts "Congrats! You don't suck that bad."
